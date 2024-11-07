0183b53d8a5c1164ea9b0c8dcd4e53fd0a9873ca
AllMightyBucket <kyubi.noyoko.rikudowich@gmail.com>
2024-11-07 05:02:04 +0300

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,13 @@
+name='Rustam'
+print(name)
+print('Age 17')
+print('NewAge 18')
+print('IsStudent:True')
+
+name='Rustam'
+print(name)
+Age=17
+print(Age)
+Years=1
+NewAge=print(Age+Years)
+IsStudent=print(True)
